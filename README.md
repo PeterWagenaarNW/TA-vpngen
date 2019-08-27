1a890f31ea5e1f7b4d58d17160cf6e3ecd68e762
Peter Wagenaar <peter.wagenaar@northwave.nl>
2019-08-27 11:57:08 +0200

    Initial working version

---
+++ README.md
@@ -0,0 +1,5 @@
+# vpn_lookup_splunk
+
+VPN lookup generator for Splunk
+
+Based on the work done on TA-asngen by @doksu (https://github.com/doksu/TA-asngen)
---
+++ bin/vpngen.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+
+from splunklib.searchcommands import dispatch, GeneratingCommand, Configuration, Option, validators
+import sys
+import os
+import ConfigParser
+import urllib2
+
+@Configuration(type='reporting')
+class VPNGenCommand(GeneratingCommand):
+
+    def generate(self):
+
+        proxies = {'http': None, 'https': None}
+
+        try:
+            cf = ConfigParser.ConfigParser()
+            cf.read(os.path.join(os.environ['SPLUNK_HOME'], 'etc/apps/VPNgen/local/vpngen.conf'))
+
+            if cf.has_section('proxies'):
+                if cf.has_option('proxies', 'https'):
+                    if len(cf.get('proxies', 'https')) > 0:
+                        proxies['https'] = cf.get('proxies', 'https')
+
+        except:
+            pass
+
+        if proxies['https'] is not None:
+            proxy = urllib2.ProxyHandler(proxies)
+            opener = urllib2.build_opener(proxy)
+            urllib2.install_opener(opener)
+
+        try:
+            url = urllib2.urlopen("https://raw.githubusercontent.com/ejrv/VPNs/master/vpn-ipv4.txt")
+        except:
+            raise Exception("Please check app proxy settings")
+
+        if url.getcode()==200:
+
+            for line in url.read().split('\n'):
+                if line.startswith('#') or not line:
+                    # ignore blank lines and lines starting with #
+                    continue
+                if '/' not in line:
+                    # CIDR notation
+                    line = line.strip() + '/32'
+                
+                yield {'ip': line, 'vpn': 'true'}
+                
+        else:
+            raise Exception("Received response: " + url.getcode())
+
+        # for name in zipfile.namelist():
+        #     entries = re.findall(r'^(\d+\.\d+\.\d+\.\d+)\/(\d+),(\d+),\"?([^\"\n]+)\"?', zipfile.open(name).read(), re.MULTILINE)
+        #     for line in entries:
+        #         yield {'ip': line[0] + "/" + line[1], 'asn': line[2], 'autonomous_system': line[3].decode('utf-8', 'ignore')}
+
+dispatch(VPNGenCommand, sys.argv, sys.stdin, sys.stdout, __name__)
